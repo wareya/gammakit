23c74fc8b89651ae7cb1ca69c32792555e9f546d
wareya <wareya@gmail.com>
2018-12-30 17:05:03 -0500

    simplify jumping.rs

--- src/interpreter/jumping.rs
+++ src/interpreter/jumping.rs
@@ -24,122 +24,89 @@ impl Interpreter
         }
         self.set_pc(function.startaddr);
         
-        if let Some(scope) = self.top_frame.scopes.last_mut()
+        let scope = self.top_frame.scopes.last_mut().ok_or_else(|| minierr("internal error: no scope in top frame despite just making it in jump_to_function (this error should be unreachable!)"))?;
+        
+        if let Some(ref name) = funcdata.name
         {
-            if let Some(ref name) = funcdata.name
-            {
-                scope.insert(name.clone(), Value::Func(Box::new(funcdata.clone())));
-            }
-            for varname in &function.varnames
-            {
-                if let Some(arg) = args.pop()
-                {
-                    scope.insert(varname.clone(), arg);
-                }
-                else
-                {
-                    return plainerr("internal error: list of arguments to provide to function was shorter than list of argument names (this error should be unreachable!)");
-                }
-            }
+            scope.insert(name.clone(), Value::Func(Box::new(funcdata.clone())));
         }
-        else
+        for varname in &function.varnames
         {
-            return plainerr("internal error: no scope in top frame despite just making it in jump_to_function (this error should be unreachable!)");
+            let arg = args.pop().ok_or_else(|| minierr("internal error: list of arguments to provide to function was shorter than list of argument names (this error should be unreachable!)"))?;
+            scope.insert(varname.clone(), arg);
         }
+        
         Ok(())
     }
     pub (crate) fn call_function(&mut self, funcdata : FuncVal, args : Vec<Value>, isexpr : bool) -> StepResult
     {
         if funcdata.internal
         {
-            if let Some(name) = funcdata.name
+            let name = funcdata.name.ok_or_else(|| minierr("internal error: function variable describing internal function is lacking its function name"))?;
+            
+            let internal_func = self.get_internal_function(&name).ok_or_else(|| minierr("internal error: tried to look up non-extant internal function after it was already referenced in a value (this should be unreachable!)"))?;
+            
+            let (ret, moved_frame) = internal_func(self, args, isexpr)?;
+            if isexpr && !self.internal_function_is_noreturn(&name)
             {
-                if let Some(internal_func) = self.get_internal_function(&name)
+                if !moved_frame
                 {
-                    let (ret, moved_frame) = internal_func(self, args, isexpr)?;
-                    if isexpr && !self.internal_function_is_noreturn(&name)
-                    {
-                        if !moved_frame
-                        {
-                            self.stack_push_val(ret);
-                        }
-                        else if let Some(frame) = self.frames.last_mut()
-                        {
-                            frame.push_val(ret);
-                        }
-                        else
-                        {
-                            return Err(Some(format!("internal error: couldn't find old frame after calling function `{}` that moves the frame; framestack has length {}", name, self.frames.len())));
-                        }
-                    }
+                    self.stack_push_val(ret);
                 }
                 else
                 {
-                    return plainerr("internal error: tried to look up non-extant internal function after it was already referenced in a value (this should be unreachable!)");
+                    let frame = self.frames.last_mut().ok_or_else(|| Some(format!("internal error: couldn't find old frame after calling function `{}` that moves the frame", name)))?;
+                    frame.push_val(ret);
                 }
-            }
-            else
-            {
-                return plainerr("internal error: function variable describing internal function is lacking its function name");
             }
         }
         else
         {
             let definition = funcdata.userdefdata.clone();
-            if let Some(defdata) = definition
+            let defdata = definition.ok_or_else(|| minierr("internal error: called a function that was not internal but didn't have definition data"))?;
+            
+            if !defdata.fromobj
             {
-                if !defdata.fromobj
-                {
-                    self.jump_to_function(&defdata, args, isexpr, &funcdata)?;
-                    return Ok(());
-                }
-                else if defdata.forcecontext != 0
-                {
-                    if let Some(inst) = self.global.instances.get(&defdata.forcecontext)
-                    {
-                        // FIXME ?
-                        if !self.global.objects.contains_key(&inst.objtype)
-                        {
-                            return Err(Some(format!("error: tried to access data from object type {} that no longer exists", inst.objtype)));
-                        }
-                        if defdata.parentobj != inst.objtype
-                        {
-                            return Err(Some(format!("error: tried to call function from object type {} in the context of an instance of object type {}", defdata.parentobj, inst.objtype)));
-                        }
-                        self.jump_to_function(&defdata, args, isexpr, &funcdata)?;
-                        self.top_frame.instancestack.push(defdata.forcecontext);
-                        return Ok(());
-                    }
-                }
-                else
+                self.jump_to_function(&defdata, args, isexpr, &funcdata)?;
+                return Ok(());
+            }
+            else if defdata.forcecontext != 0
+            {
+                if let Some(inst) = self.global.instances.get(&defdata.forcecontext)
                 {
                     // FIXME ?
-                    if let Some(instance) = self.top_frame.instancestack.last().cloned()
+                    if !self.global.objects.contains_key(&inst.objtype)
                     {
-                        if let Some(inst) = self.global.instances.get(&instance)
-                        {
-                            if !self.global.objects.contains_key(&inst.objtype)
-                            {
-                                return Err(Some(format!("error: tried to access data from object type {} that no longer exists", inst.objtype)));
-                            }
-                            if defdata.parentobj != inst.objtype
-                            {
-                                return Err(Some(format!("error: tried to call function from object type {} in the context of an instance of object type {}", defdata.parentobj, inst.objtype)));
-                            }
-                            self.jump_to_function(&defdata, args, isexpr, &funcdata)?; // opens a new frame, changing top_frame to a clean slate
-                            self.top_frame.instancestack.push(instance);
-                            return Ok(());
-                        }
-                        else
-                        {
-                            return plainerr("internal error: tried to look for a variable inside of an instance that no longer exists (this might not be an error state!)");
-                        }
+                        return Err(Some(format!("error: tried to access data from object type {} that no longer exists", inst.objtype)));
                     }
+                    if defdata.parentobj != inst.objtype
+                    {
+                        return Err(Some(format!("error: tried to call function from object type {} in the context of an instance of object type {}", defdata.parentobj, inst.objtype)));
+                    }
+                    self.jump_to_function(&defdata, args, isexpr, &funcdata)?;
+                    self.top_frame.instancestack.push(defdata.forcecontext);
+                    return Ok(());
                 }
             }
             else
             {
-                return plainerr("internal error: called a function that was not internal but didn't have definition data");
+                // FIXME ?
+                if let Some(instance) = self.top_frame.instancestack.last().cloned()
+                {
+                    let inst = self.global.instances.get(&instance).ok_or_else(|| minierr("internal error: tried to look for a variable inside of an instance that no longer exists (this might not be an error state!)"))?;
+                    
+                    if !self.global.objects.contains_key(&inst.objtype)
+                    {
+                        return Err(Some(format!("error: tried to access data from object type {} that no longer exists", inst.objtype)));
+                    }
+                    if defdata.parentobj != inst.objtype
+                    {
+                        return Err(Some(format!("error: tried to call function from object type {} in the context of an instance of object type {}", defdata.parentobj, inst.objtype)));
+                    }
+                    self.jump_to_function(&defdata, args, isexpr, &funcdata)?; // opens a new frame, changing top_frame to a clean slate
+                    self.top_frame.instancestack.push(instance);
+                    return Ok(());
+                }
             }
         }
         Ok(())
