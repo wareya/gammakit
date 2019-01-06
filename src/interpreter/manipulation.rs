f6e9fa76134312ac2b0415496c86ee757504577c
wareya <wareya@gmail.com>
2019-01-06 07:59:10 -0500

    use read_usize in manipulation.rs

--- src/interpreter/manipulation.rs
+++ src/interpreter/manipulation.rs
@@ -111,7 +111,7 @@ impl Interpreter
         
         let argcount = self.read_u16()?;
         
-        let bodylen = unpack_u64(&self.pull_from_code(8)?)? as usize;
+        let bodylen = self.read_usize()?;
         
         let mut args = Vec::<String>::new();
         for _ in 0..argcount
@@ -151,7 +151,7 @@ impl Interpreter
         
         let argcount = self.read_u16()?;
         
-        let bodylen = unpack_u64(&self.pull_from_code(8)?)? as usize;
+        let bodylen = self.read_usize()?;
         
         let mut args = Vec::<String>::new();
         for _ in 0..argcount
