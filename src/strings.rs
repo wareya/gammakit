e61850f6772fb5b004e29111c1417f242baae735
wareya <wareya@gmail.com>
2019-01-07 22:27:17 -0500

    simplify match in strings.rs

--- src/strings.rs
+++ src/strings.rs
@@ -18,18 +18,16 @@ pub (crate) fn unescape(text: &str) -> String
         if c != '\\'
         {
             ret.push(c);
+            continue;
         }
-        else if let Some(c2) = chars.pop_front()
+        match chars.pop_front()
         {
-            match c2
-            {
-                '\\' => ret.push(c),
-                'n' => ret.push('\n'),
-                'r' => ret.push('\r'),
-                't' => ret.push('\t'),
-                '"' => ret.push('"'),
-                _ => ret.extend(&[c, c2])
-            }
+            Some('\\') => ret.push(c),
+            Some('n') => ret.push('\n'),
+            Some('r') => ret.push('\r'),
+            Some('t') => ret.push('\t'),
+            Some('"') => ret.push('"'),
+            _ => ret.extend(&[c, c2])
         }
     }
     ret
