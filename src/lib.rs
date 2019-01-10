f2d51d9dcad85f91bfc48902379a8c2cdea316b2
wareya <wareya@gmail.com>
2019-01-09 23:21:15 -0500

    add compilation helper function

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,22 @@ mod interpreter;
 
 pub use crate::{parser::*, compiler::*, interpreter::*};
 
+use std::rc::Rc;
+
+impl Parser {
+    /// optional helper function to simplify the process of compiling from text to bytecode
+    pub fn give_me_bytecode(&mut self, text: &String) -> Result<Rc<Vec<u8>>, String>
+    {
+        let program_lines : Vec<String> = text.lines().map(|x| x.to_string()).collect();
+        
+        let tokens = self.tokenize(&program_lines, false)?;
+        
+        let ast = self.parse_program(&tokens, &program_lines, false)?.ok_or_else(|| "failed to parse program".to_string())?;
+        
+        Ok(Rc::new(compile_bytecode(&ast)?))
+    }
+}
+
 /// Gammakit is a high-level scripting language meant for games.
 ///
 /// WARNING: Gammakit is the bespoke programming language of a toy game engine. It is not for general use.
@@ -19,22 +35,16 @@ pub use crate::{parser::*, compiler::*, interpreter::*};
 ///
 /// If, for some reason, you decide to use gammkit, make a hard fork of it. You're gonna want to change stuff anyway.
 ///
-/// 1) Load a grammar into a string (e.g. grammarsimple.txt)
-/// 2) Call Parser::new_from_default() to get a new parser initialized with the default parser
-/// 3) Load a program into a vector of Strings. (This will probably be changed to a mere single string later.)
-/// 4) Tokenize the program with parser.tokenize(&lines, false)
-/// 5) Parse the token list with parser.parse_programs(&tokens, &lines, false)
-/// 6) Compile to bytecode with compile_bytecode(&ast)
-/// 7) Create an interpreter with Interpreter::new(&Rc::new(code), Some(parser)) or similar
-/// 8) Optional: insert the default binding functions with interpreter.insert_default_internal_functions()
-/// 9) Run interpreter.step() until it returns Err. Err(None) indicates graceful exit, Err(Some(String))) indicates an error.
+/// 1) Call Parser::new_from_default() to get a new parser initialized with the default parser
+/// 2) Compile program text to bytecode with parser.give_me_bytecode(text) (a helper function)
+/// 3) Create an interpreter with Interpreter::new(&code, Some(parser)) or similar
+/// 4) Optional: insert the default binding functions with interpreter.insert_default_internal_functions()
+/// 5) Run interpreter.step() until it returns Err. Err(None) indicates graceful exit, Err(Some(String))) indicates an error.
 
 #[cfg(test)]
 mod tests {
     use std::fs::File;
     use std::io::Read;
-    use std::vec::Vec;
-    use std::rc::Rc;
     
     use super::*;
     
@@ -43,17 +53,10 @@ mod tests {
     {
         let mut parser = Parser::new_from_default()?;
 
-        let mut file2 = File::open("program.txt").or_else(|_| Err("failed to open program".to_string()))?;
-        let mut contents2 = String::new();
-        file2.read_to_string(&mut contents2).or_else(|_| Err("failed to read program into memory".to_string()))?;
+        let mut program = String::new();
+        File::open("program.txt").or_else(|_| Err("failed to open program".to_string()))?.read_to_string(&mut program).or_else(|_| Err("failed to read program into memory".to_string()))?;
         
-        let program_lines : Vec<String> = contents2.lines().map(|x| x.to_string()).collect();
-        
-        let tokens = parser.tokenize(&program_lines, false)?;
-        
-        let ast = parser.parse_program(&tokens, &program_lines, false)?.ok_or_else(|| "failed to parse program".to_string())?;
-        
-        let code = Rc::new(compile_bytecode(&ast)?);
+        let code = parser.give_me_bytecode(&program)?;
         
         let mut interpreter = Interpreter::new(&code, Some(parser));
         interpreter.insert_default_internal_functions();
